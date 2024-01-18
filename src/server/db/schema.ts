74e15f08fa434ad560e002656051d069bb3e09dc
Touha Zohair <touha98@gmail.com>
2024-01-19 00:55:29 +0600

    Add tasks table and task relations

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,13 @@
-import { bigint, boolean, timestamp, varchar } from "drizzle-orm/mysql-core";
+import {
+  bigint,
+  boolean,
+  index,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
 import { mysqlTable } from "@/server/db/util";
+import { relations } from "drizzle-orm";
 
 export const users = mysqlTable("users", {
   id: varchar("id", { length: 15 }).primaryKey(),
@@ -43,4 +50,30 @@ export const emailVerificationTokens = mysqlTable("email_verification_tokens", {
   userId: varchar("user_id", { length: 15 }).notNull(),
   expires: bigint("expires", { mode: "number" }).notNull(),
 });
- 
+
+export const tasks = mysqlTable(
+  "tasks",
+  {
+    id: varchar("id", { length: 15 }).primaryKey(),
+    userId: varchar("user_id", { length: 15 }).notNull(),
+    title: varchar("title", { length: 255 }).notNull(),
+    description: varchar("description", { length: 255 }),
+    status: varchar("status", {
+      length: 5,
+      enum: ["todo", "doing", "done"],
+    })
+      .default("todo")
+      .notNull(),
+    tags: varchar("tags", { length: 255 }),
+    archived: boolean("archived").default(false).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").onUpdateNow(),
+  },
+  (t) => ({
+    userIdx: index("user_idx").on(t.userId),
+  }),
+);
+
+export const taskRelations = relations(tasks, ({ one }) => ({
+  user: one(users),
+}));

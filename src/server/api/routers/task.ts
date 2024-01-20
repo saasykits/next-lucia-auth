b89625a7b3c1004213207139d46f49079556f14e
Touha Zohair <touha98@gmail.com>
2024-01-20 09:26:16 +0600

    Add task router and CRUD operations for tasks

---
+++ src/server/api/routers/task.ts
@@ -0,0 +1,69 @@
+import crypto from "crypto";
+import { z } from "zod";
+import { and, desc, eq } from "drizzle-orm";
+import { tasks } from "@/server/db/schema";
+import { protectedProcedure, createTRPCRouter } from "../trpc";
+
+export const taskRouter = createTRPCRouter({
+  list: protectedProcedure.query(async ({ ctx }) => {
+    const data = await ctx.db.query.tasks.findMany({
+      where: and(
+        eq(tasks.userId, ctx.session.user.userId),
+        eq(tasks.archived, false),
+      ),
+      orderBy: desc(tasks.createdAt),
+    });
+    return data;
+  }),
+  archivedList: protectedProcedure.query(({ ctx }) =>
+    ctx.db.query.tasks.findMany({
+      where: and(
+        eq(tasks.userId, ctx.session.user.userId),
+        eq(tasks.archived, true),
+      ),
+      orderBy: desc(tasks.createdAt),
+    }),
+  ),
+  create: protectedProcedure
+    .input(
+      z.object({
+        title: z.string().trim().min(1).max(255),
+        description: z.string().trim().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const id = generateRandomString();
+
+      await ctx.db.insert(tasks).values({
+        id,
+        userId: ctx.session.user.userId,
+        title: input.title,
+        description: input.description,
+      });
+      return { id };
+    }),
+  delete: protectedProcedure
+    .input(z.string().trim().length(15))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .delete(tasks)
+        .where(
+          and(eq(tasks.id, input), eq(tasks.userId, ctx.session.user.userId)),
+        );
+      return { id: input };
+    }),
+});
+
+function generateRandomString(length = 15) {
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const randomBytes = crypto.randomBytes(length);
+  let randomString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = randomBytes[i]! % characters.length;
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+}

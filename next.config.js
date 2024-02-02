ee11dad5fe201d29858034b73001c255f9d640c5
Touha Zohair <touha98@gmail.com>
2024-02-02 15:43:37 +0600

    Use Argon2id for password hashing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    return config;
+  },
+};
 
 export default config;

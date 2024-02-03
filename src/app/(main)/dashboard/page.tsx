8e42cebe9220254db6b8a3d25b343d6b51c653c4
Touha Zohair <touha98@gmail.com>
2024-02-03 14:43:58 +0600

    remove task

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,16 +1,6 @@
-import { NewTaskDialog } from "./new-task";
-import { Tasks } from "./tasks";
-
 function DashboardPage() {
   return (
-    <div className="container mx-auto min-h-[calc(100vh-100px)] p-2">
-      <div className="flex items-center py-4">
-        <h1 className="w-full text-3xl font-bold">Your Tasks</h1>
-        <NewTaskDialog />
-      </div>
-      <div className="h-10"></div>
-      <Tasks />
-    </div>
+    <div className="container mx-auto min-h-[calc(100vh-100px)] p-2"></div>
   );
 }
 
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,8 @@
 import { createTRPCRouter } from "./trpc";
 import { userRouter } from "./routers/user";
-import { taskRouter } from "./routers/task";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
-  task: taskRouter,
 });
 
 export type AppRouter = typeof appRouter;

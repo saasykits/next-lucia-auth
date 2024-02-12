6ab0d7327124dd00aecfc00ae4bc82eb3b5e6fd0
Touha Zohair <touha98@gmail.com>
2024-02-12 12:42:01 +0600

    remov duplicate trpc methods

--- src/server/api/routers/stripe.ts
+++ src/server/api/routers/stripe.ts
@@ -1,10 +1,6 @@
 import { subscriptionPlans } from "@/config/subscriptions";
 import { absoluteUrl } from "@/lib/utils";
 import { manageSubscriptionSchema } from "@/lib/validators/stripe";
-import { posts } from "@/server/db/schema";
-import { eq } from "drizzle-orm";
-import { generateId } from "lucia";
-import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { stripe } from "@/lib/stripe";
 
@@ -109,97 +105,4 @@ export const stripeRouter = createTRPCRouter({
         url: stripeSession.url,
       };
     }),
-
-  list: protectedProcedure
-    .input(
-      z.object({
-        page: z.number().int().default(1),
-        perPage: z.number().int().default(12),
-      }),
-    )
-    .query(({ ctx, input }) =>
-      ctx.db.query.posts.findMany({
-        where: (table, { eq }) => eq(table.status, "published"),
-        offset: (input.page - 1) * input.perPage,
-        limit: input.perPage,
-        orderBy: (table, { desc }) => desc(table.createdAt),
-        columns: {
-          id: true,
-          title: true,
-          excerpt: true,
-          status: true,
-          createdAt: true,
-        },
-        with: { user: { columns: { email: true } } },
-      }),
-    ),
-  get: protectedProcedure.input(z.string()).query(({ ctx, input }) =>
-    ctx.db.query.posts.findFirst({
-      where: (table, { eq }) => eq(table.id, input),
-      with: { user: { columns: { email: true } } },
-    }),
-  ),
-
-  create: protectedProcedure
-    .input(
-      z.object({
-        title: z.string().min(3).max(255),
-        excerpt: z.string().min(3).max(255),
-        content: z.string().min(3),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      const id = generateId(15);
-      await ctx.db.insert(posts).values({
-        id,
-        userId: ctx.user.id,
-        title: input.title,
-        excerpt: input.excerpt,
-        content: input.content,
-      });
-      return { id };
-    }),
-
-  update: protectedProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        title: z.string().min(3).max(255),
-        excerpt: z.string().min(3).max(255),
-        content: z.string().min(3),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db
-        .update(posts)
-        .set({
-          title: input.title,
-          excerpt: input.excerpt,
-          content: input.content,
-        })
-        .where(eq(posts.id, input.id));
-    }),
-
-  myPosts: protectedProcedure
-    .input(
-      z.object({
-        page: z.number().int().default(1),
-        perPage: z.number().int().default(12),
-      }),
-    )
-    .query(({ ctx, input }) =>
-      ctx.db.query.posts.findMany({
-        where: (table, { eq }) => eq(table.userId, ctx.user.id),
-        offset: (input.page - 1) * input.perPage,
-        limit: input.perPage,
-        orderBy: (table, { desc }) => desc(table.createdAt),
-        columns: {
-          id: true,
-          title: true,
-          excerpt: true,
-          status: true,
-          createdAt: true,
-        },
-      }),
-    ),
 });

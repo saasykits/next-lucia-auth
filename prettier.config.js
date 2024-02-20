37131a9f78f5bfdedde6876c2899a2f3380d82d5
Touha Zohair <touha98@gmail.com>
2024-02-21 00:37:19 +0600

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
 };
 
 export default config;

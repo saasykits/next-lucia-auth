c8f4b3e25d1d10f3666c25a0df71abd39494d589
Touha Zohair <touha98@gmail.com>
2024-02-21 09:49:52 +0600

    add key to map child

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -39,8 +39,7 @@ const features = [
   },
   {
     name: "Authentication",
-    description:
-      "Credential authentication with password reset and email validation",
+    description: "Credential authentication with password reset and email validation",
     logo: LuciaAuth,
   },
   {
@@ -78,20 +77,19 @@ const features = [
 const HomePage = () => {
   return (
     <>
-      <section className="mx-auto grid min-h-[calc(100vh-300px)] max-w-5xl flex-col  justify-center gap-4 py-10 md:py-12 text-center  items-center">
+      <section className="mx-auto grid min-h-[calc(100vh-300px)] max-w-5xl flex-col  items-center justify-center gap-4 py-10 text-center  md:py-12">
         <div className="p-4">
           <div className="mb-10 flex items-center justify-center gap-3">
             <NextjsIcon className="h-[52px] w-[52px]" />
             <PlusIcon className="h-8 w-8" />
             <LuciaAuth className="h-14 w-14" />
           </div>
-          <h1 className="text-balance text-center  bg-gradient-to-tr from-black/70 via-black to-black/60 dark:from-zinc-400/10 dark:via-white/90 dark:to-white/20 bg-clip-text text-transparent text-3xl font-bold  sm:text-5xl md:text-6xl lg:text-7xl">
+          <h1 className="text-balance bg-gradient-to-tr  from-black/70 via-black to-black/60 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-zinc-400/10 dark:via-white/90 dark:to-white/20  sm:text-5xl md:text-6xl lg:text-7xl">
             Next.js Lucia Auth Starter Template
           </h1>
           <p className="text-balance mb-10 mt-4 text-center text-muted-foreground md:text-lg lg:text-xl">
-            A Next.js Authentication starter template (password reset, email
-            validation and oAuth). Includes Lucia, Drizzle, tRPC, Stripe,
-            tailwindcss, shadcn-ui and react-email.
+            A Next.js Authentication starter template (password reset, email validation and oAuth).
+            Includes Lucia, Drizzle, tRPC, Stripe, tailwindcss, shadcn-ui and react-email.
           </p>
           <div className="mb-10">
             <div className="mx-auto max-w-[430px]">
@@ -117,13 +115,17 @@ const HomePage = () => {
             <a id="features"></a> Features
           </h1>
           <p className="text-balance mb-10 text-center text-muted-foreground md:text-lg lg:text-xl">
-            This starter template is a guide to help you get started with
-            Next.js for large scale applications. Feel free to add or remove
-            features to suit your needs.
+            This starter template is a guide to help you get started with Next.js for large scale
+            applications. Feel free to add or remove features to suit your needs.
           </p>
           <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
-            {features.map((feature) => (
-              <CardSpotlight name={feature.name} description={feature.description} logo={<feature.logo className='w-12 h-12' />} />
+            {features.map((feature, i) => (
+              <CardSpotlight
+                key={i}
+                name={feature.name}
+                description={feature.description}
+                logo={<feature.logo className="h-12 w-12" />}
+              />
             ))}
           </div>
         </div>

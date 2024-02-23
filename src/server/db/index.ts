b16e4787fb2c2d075736e110b9943b2dbefb5a80
Touha Zohair <touha98@gmail.com>
2024-02-23 15:41:39 +0600

    db `Connection` -> `Client`

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { Connection } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 
 import { env } from "@/env";
 import * as schema from "./schema";
 
-export const connection = new Connection({ url: env.DATABASE_URL });
+export const connection = new Client({ url: env.DATABASE_URL });
 
 export const db = drizzle(connection, { schema });

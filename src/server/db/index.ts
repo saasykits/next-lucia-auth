cb42e43718c1946e14eb1bc32ac7bd45af402ce0
Touha Zohair <39002455+iamtouha@users.noreply.github.com>
2024-04-04 11:49:37 +0600

    Update database connection and migration to use PostgreSQL

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,11 @@
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { Client } from "@planetscale/database";
-
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import { env } from "@/env";
 import * as schema from "./schema";
 
-export const connection = new Client({ url: env.DATABASE_URL });
+export const connection = postgres(env.DATABASE_URL, {
+  max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
+  // idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
+});
 
 export const db = drizzle(connection, { schema });

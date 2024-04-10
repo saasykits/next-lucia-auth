3588ddb117993e4c86b69676dc66479ff962080d
Ryan Lee <drdgvhbh@gmail.com>
2024-04-10 00:40:50 -0400

    chore: fix absolute routes

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -4,6 +4,7 @@ import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { env } from "@/env.js";
 import { db } from "@/server/db";
 import { sessions, users, type User as DbUser } from "@/server/db/schema";
+import { absoluteUrl } from "@/lib/utils"
 
 // Uncomment the following lines if you are using nodejs 18 or lower. Not required in Node.js 20, CloudFlare Workers, Deno, Bun, and Vercel Edge Functions.
 // import { webcrypto } from "node:crypto";
@@ -39,7 +40,7 @@ export const lucia = new Lucia(adapter, {
 export const discord = new Discord(
   env.DISCORD_CLIENT_ID,
   env.DISCORD_CLIENT_SECRET,
-  env.NEXT_PUBLIC_APP_URL + "/login/discord/callback",
+  absoluteUrl("/login/discord/callback")
 );
 
 declare module "lucia" {

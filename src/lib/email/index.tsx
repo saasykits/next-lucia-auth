bfde4554cefece8c197d27950f6c702c1d28c5e8
Touha Zohair <39002455+iamtouha@users.noreply.github.com>
2024-04-16 10:56:09 +0600

    update sendMail function

---
+++ src/lib/email/index.tsx
@@ -0,0 +1,66 @@
+import "server-only";
+
+import { EmailVerificationTemplate } from "./templates/email-verification";
+import { ResetPasswordTemplate } from "./templates/reset-password";
+import { render } from "@react-email/render";
+import { env } from "@/env";
+import { EMAIL_SENDER } from "@/lib/constants";
+import { createTransport, type TransportOptions } from "nodemailer";
+import type { ComponentProps } from "react";
+
+export enum EmailTemplate {
+  EmailVerification = "EmailVerification",
+  PasswordReset = "PasswordReset",
+}
+
+export type PropsMap = {
+  [EmailTemplate.EmailVerification]: ComponentProps<typeof EmailVerificationTemplate>;
+  [EmailTemplate.PasswordReset]: ComponentProps<typeof ResetPasswordTemplate>;
+};
+
+const getEmailTemplate = <T extends EmailTemplate>(template: T, props: PropsMap[NoInfer<T>]) => {
+  switch (template) {
+    case EmailTemplate.EmailVerification:
+      return {
+        subject: "Verify your email address",
+        body: render(
+          <EmailVerificationTemplate {...(props as PropsMap[EmailTemplate.EmailVerification])} />,
+        ),
+      };
+    case EmailTemplate.PasswordReset:
+      return {
+        subject: "Verify your email address",
+        body: render(
+          <ResetPasswordTemplate {...(props as PropsMap[EmailTemplate.PasswordReset])} />,
+        ),
+      };
+    default:
+      throw new Error("Invalid email template");
+  }
+};
+
+const smtpConfig = {
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
+  auth: {
+    user: env.SMTP_USER,
+    pass: env.SMTP_PASSWORD,
+  },
+};
+
+const transporter = createTransport(smtpConfig as TransportOptions);
+
+export const sendMail = async <T extends EmailTemplate>(
+  to: string,
+  template: T,
+  props: PropsMap[NoInfer<T>],
+) => {
+  if (env.NODE_ENV !== "production") {
+    console.log("📨 Email sent to:", to, "with template:", template, "and props:", props);
+    return;
+  }
+
+  const { subject, body } = getEmailTemplate(template, props);
+
+  return transporter.sendMail({ from: EMAIL_SENDER, to, subject, html: body });
+};

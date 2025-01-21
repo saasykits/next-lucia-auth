6adcfe9eafcf6a0afd3c1556f1a7a0f33221d448
Touha Zohair <touha98@gmail.com>
2025-01-21 07:40:12 +0000

    update footer links to reflect new repository and organization; adjust footer styling for better spacing

--- src/app/(main)/_components/footer.tsx
+++ src/app/(main)/_components/footer.tsx
@@ -1,21 +1,21 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 import { CodeIcon } from "@radix-ui/react-icons";
 
-const githubUrl = "https://github.com/iamtouha/next-lucia-auth";
-const twitterUrl = "https://twitter.com/iamtouha";
+const repoUrl = "https://github.com/saasykits/nextjs-session-auth";
+const orgUrl = "https://github.com/saasykits";
 
 export const Footer = () => {
   return (
-    <footer className="mt-6 px-4 py-6">
+    <footer className="mt-2 px-4 py-2">
       <div className="container flex items-center p-0">
         <CodeIcon className="mr-2 h-6 w-6" />
         <p className="text-sm">
           Built by{" "}
-          <a className="underline underline-offset-4" href={twitterUrl}>
-            iamtouha
+          <a className="underline underline-offset-4" href={orgUrl}>
+            saasykits
           </a>
           . Get the source code from{" "}
-          <a className="underline underline-offset-4" href={githubUrl}>
+          <a className="underline underline-offset-4" href={repoUrl}>
             GitHub
           </a>
           .
